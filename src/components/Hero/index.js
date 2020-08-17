a05980a52b2bccf1e4e4d1ee7eef7ae4e1f0b215
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-08-17 01:31:49 -0300

    Hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 function Hero () {
     return (
         <section id='home' >
-            <h1> Hero </h1>
-        
+            <Fade left duration={1000} delay={500} distance="30px">
+                <h1 className="hero-title">
+                    Hi, my name is
+                    <span className="destak-text"> Vitor Bigelli </span>
+                    <br />
+                    I'm a Full Stack Web Developer
+                </h1>
+            </Fade>                        
         </section>
     )
 }
