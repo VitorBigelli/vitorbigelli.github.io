678dedbb86d261330cad8b6e3a3aa9daa342c020
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-08-17 11:34:05 -0300

    Updates

---
+++ precache-manifest.efa28e76fb7e7ac72876503f3b51d778.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a160bd201b9b454b8484d5242b21ad",
+    "url": "/vitorbigelli.github.io/index.html"
+  },
+  {
+    "revision": "0ec26bfe18d1e932b387",
+    "url": "/vitorbigelli.github.io/static/css/2.178dc2c7.chunk.css"
+  },
+  {
+    "revision": "4953bdbd825cccc5bdf9",
+    "url": "/vitorbigelli.github.io/static/css/main.20f441bf.chunk.css"
+  },
+  {
+    "revision": "0ec26bfe18d1e932b387",
+    "url": "/vitorbigelli.github.io/static/js/2.7336a2d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vitorbigelli.github.io/static/js/2.7336a2d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4953bdbd825cccc5bdf9",
+    "url": "/vitorbigelli.github.io/static/js/main.7313eacd.chunk.js"
+  },
+  {
+    "revision": "649add106404055fce40",
+    "url": "/vitorbigelli.github.io/static/js/runtime-main.20333f23.js"
+  },
+  {
+    "revision": "8913b86add0095f7d045c513e6590d64",
+    "url": "/vitorbigelli.github.io/static/media/profile.8913b86a.jpg"
+  },
+  {
+    "revision": "77fd660096f60df224c7a286fb0abe99",
+    "url": "/vitorbigelli.github.io/static/media/project_1.77fd6600.png"
+  },
+  {
+    "revision": "8ad7a0dbe882510d6046ae6db3008dda",
+    "url": "/vitorbigelli.github.io/static/media/project_2.8ad7a0db.png"
+  },
+  {
+    "revision": "d94871331399994f0b2a654188dc3334",
+    "url": "/vitorbigelli.github.io/static/media/project_3.d9487133.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/vitorbigelli.github.io/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/vitorbigelli.github.io/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/vitorbigelli.github.io/static/media/revicons.2feb69cc.eot"
+  }
+]);
