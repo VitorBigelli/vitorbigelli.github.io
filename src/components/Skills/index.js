e82ebb4b6c99eafe6b43e7e5313d4810449d06f6
Vitor Bigelli <vitorbigelli@hotmail.com>
2020-08-17 11:24:28 -0300

    Add skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,10 +1,116 @@
 import React from 'react'
+import {
+    DiNodejsSmall,
+    DiReact, 
+    DiBootstrap,
+    DiJavascript, 
+    DiPython,
+    DiDocker,
+    DiMongodb,
+    DiHtml5,
+    DiCss3,
+    DiGoogleCloudPlatform,
+    DiDigitalOcean,
+    DiGit,
+    DiNginx,
+} from 'react-icons/di'
+import {
+    FaAws
+} from 'react-icons/fa'
+import { Icon, InlineIcon } from '@iconify/react';
+import neo4jIcon from '@iconify/icons-simple-icons/neo4j';
+import rabbitmq from '@iconify/icons-simple-icons/rabbitmq';
+
+const skills = {
+    front: [
+        { name: 'React', className: 'react', icon: () => <DiReact /> }, 
+        { name: 'Bootstrap', className: 'bootstrap', icon: () => <DiBootstrap /> }, 
+        { name: 'Javascript', className: 'javascript', icon: () => <DiJavascript /> }, 
+        { name: 'HTML 5', className: 'html', icon: () => <DiHtml5 /> }, 
+        { name: 'CSS 3', className: 'css', icon: () => <DiCss3 /> }, 
+    ], 
+    back: [
+        { name: 'Python', className: 'python', icon: () => <DiPython /> }, 
+        { name: 'NodeJS', className: 'nodejs', icon: () => <DiNodejsSmall /> }, 
+        { name: 'RabbitMQ', className: 'rabbitmq', icon: () => <Icon icon={rabbitmq} /> }, 
+    ],
+    databases: [
+        { name: 'MongoDB', className: 'mongodb', icon: () => <DiMongodb /> }, 
+        { name: 'Neo4J', className: 'neo4j', icon: () => <Icon icon={neo4jIcon} /> }, 
+    ], 
+    devops: [
+        { name: 'Git', className: 'git', icon: () => <DiGit /> }, 
+        { name: 'NGinx', className: 'nginx', icon: () => <DiNginx /> }, 
+        { name: 'Google Cloud', className: 'google-cloud', icon: () => <DiGoogleCloudPlatform /> }, 
+        { name: 'Digital Ocean', className: 'digital-ocean', icon: () => <DiDigitalOcean /> }, 
+        { name: 'AWS', className: 'aws', icon: () => <FaAws /> }, 
+        { name: 'Docker', className: 'docker', icon: () => <DiDocker /> }, 
+    ]
+}
 
 function Skills () {
     return (
         <section id='skills' >
-            <h1> Skills </h1>
-        
+            <h1> Skills </h1> 
+            <h3> Front-end </h3>  
+            <ul className='flex-row'> 
+                {
+                    skills.front.map( (s, i) => {
+                        return (
+                            <li className={s.className}> 
+                                { s.icon() }
+                                <span className='tooltip'> { s.name }</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <h3> Back-end </h3>
+            <ul className='flex-row'> 
+                {
+                    skills.back.map( (s, i) => {
+                        return (
+                            <li className={s.className}> 
+                                { s.icon() }
+                                <span className='tooltip'> { s.name }</span>
+                            </li>
+                        )
+                    })
+                }
+            
+            </ul>
+            
+            <h3> Databases </h3>
+            <ul className='flex-row'>
+                
+                {
+                    skills.databases.map( (s, i) => {
+                        return (
+                            <li className={s.className}> 
+                                { s.icon() }
+                                <span className='tooltip'> { s.name }</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+
+            <h3> DevOps, Deployment & Cloud </h3>     
+            <ul className='flex-row'> 
+                
+                {
+                    skills.devops.map( (s, i) => {
+                        return (
+                            <li className={s.className}> 
+                                { s.icon() }
+                                <span className='tooltip'> { s.name }</span>
+                            </li>
+                        )
+                    })
+                }
+            
+            </ul>
         </section>
     )
 }
