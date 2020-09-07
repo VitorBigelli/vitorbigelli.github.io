7c4acd8db8b9b88a4a614e4a20cebe6355b1d62e
Vitor <bigellivitor@gmail.com>
2020-09-07 10:13:53 -0300

    Add my-reads project to portfolio

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,7 @@ import 'react-multi-carousel/lib/styles.css';
 import project_1 from '../../assets/project_1.png'
 import project_3 from '../../assets/project_3.png'
 import project_2 from '../../assets/project_2.png'
+import project_4 from '../../assets/project_4.png'
 import Flip from 'react-reveal/Flip'
 
 function Projects () {
@@ -24,12 +25,19 @@ function Projects () {
                 <p>Full-Stack Web Appp Development from the biggest rare diseases graph. Raras.org it's an open source project created with the purpouse of bringing advances in  in artificial ingellience and find answers for rare diseases.</p>
             </div>
             <div className='project-container'>
+                <h2> My Reads</h2>
+                <Flip left>
+                <img src={project_4}  alt='Vitor Bigelli | vitorbigelli | My Reads'/>
+                </Flip>
+                <p>React Static App to manage "read", "currently reading" and "want to read" books. Add books by just passing its Amazon Store URL.</p>
+            </div>
+            {/* <div className='project-container'>
                 <h2> SanarFlix - Caption Editor</h2>
                 <Flip left>
                 <img src={project_3}  alt='Vitor Bigelli | vitorbigelli | DeepWild'/>
                 </Flip>
                 <p>Full-Stack Web App development for editing subtitles for the SanarFlix medical video platform.</p>
-            </div>
+            </div> */}
         </section>
     )
 }
